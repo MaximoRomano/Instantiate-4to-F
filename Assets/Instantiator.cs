dfd1b76d46892167740d33684b643ef8d9d62104
MaximoRomano <102158982+MaximoRomano@users.noreply.github.com>
2022-05-10 10:42:08 -0300

    almost finished

--- Assets/Instantiator.cs
+++ Assets/Instantiator.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Instantiator : MonoBehaviour
 {
-    public GameObject objectToClone;        
+    public GameObject objectToClone;
+    public int CloneAmount;
+    public Text txtcloneamount;
 
     public void CloneObject()
     {
 
-        Instantiate(objectToClone);
+        CloneAmount = int.Parse(txtcloneamount.text);
+        int i = 0;
+        while (i < CloneAmount)
+        {
+            i++;
+        }
 
     }
 }
---
+++ Assets/scripts/inputmanager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputmanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public InputField ifusername;
+    public Text txtusername;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UserGreetings()
+    {
+        Debug.Log( int.Parse(txtusername.text) *2);
+    }
+}
